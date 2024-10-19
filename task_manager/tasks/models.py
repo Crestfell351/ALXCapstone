8d225bf4b0768ced39831e0a9c16bb29f95c83ea
Aphiwe Makhathini <116758736+Crestfell351@users.noreply.github.com>
2024-10-19 16:47:33 +0000

    Added models for tasks

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateField()
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES, default='pending')
+    completed_at = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
