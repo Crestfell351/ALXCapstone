6aef863cba2ac4fbd0eeccfa0cb44a49e200287e
Aphiwe Makhathini <116758736+Crestfell351@users.noreply.github.com>
2024-10-20 17:49:32 +0000

    test

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Task Management API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),
+    path('', home),  # Add this line to handle the root URL
 ]
 
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -9,6 +9,9 @@ class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['status', 'priority', 'due_date']
+    ordering_fields = ['due_date', 'priority']
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
